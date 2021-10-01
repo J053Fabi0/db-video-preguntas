33a724eebc0f2600532efdddcc925cdac32b233f
J053_Fabi0 <7194669-J053_Fabi0@users.noreply.gitlab.com>
2021-10-01 16:13:04 -0600

    Bug fix

--- main.js
+++ main.js
@@ -21,6 +21,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(function (_, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(routes);
 

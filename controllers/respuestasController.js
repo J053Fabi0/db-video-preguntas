e3d373ee825faa3e94046a760e65b8189bda8852
J053_Fabi0 <7194669-J053_Fabi0@users.noreply.gitlab.com>
2021-10-01 16:38:25 -0600

    Añadida la fecha en la que el usuario fue creado

--- controllers/respuestasController.js
+++ controllers/respuestasController.js
@@ -71,7 +71,12 @@ module.exports.setRespuesta = async ({ params: { id }, body }, res) => {
     const user = await respuestasDB.count({ _id: id });
     if (!user) {
       // Insertar el nuevo usuario
-      await respuestasDB.insert({ _id: id, terminoVideo: false, respuestas: {} });
+      await respuestasDB.insert({
+        _id: id,
+        terminoVideo: false,
+        respuestas: {},
+        fecha: new Date().toLocaleString().split(" ")[0],
+      });
       // incrementar los usuarios totales
       await respuestasDB.update({ _id: "usuarios_totales" }, { $inc: { usuarios_totales: 1 } });
     }
